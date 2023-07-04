c8ccc809b43021b9d1b1b3c7660356ca3ab5ea3c
unknown <rafaelmateostorres@gmail.com>
2023-07-04 19:04:35 +0200

    Añade h2 y carga inicial

---
+++ src/main/java/com/commerce/ecommercerafa/ECommerceRafaApplication.java
@@ -0,0 +1,13 @@
+package com.commerce.ecommercerafa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceRafaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceRafaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commerce/ecommercerafa/ECommerceRafaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commerce.ecommercerafa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceRafaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
